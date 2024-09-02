75713d45d4bc9839bd99ec4474ca0999d87d160d
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2024-09-03 02:22:33 +0700

    Add two helper functions that repeatedly reads until condition met

---
+++ include/cppread/read_repeat.hpp
@@ -0,0 +1,100 @@
+#ifndef CPPREAD_READ_REPEAT_HPP
+#define CPPREAD_READ_REPEAT_HPP
+
+#include "cppread/read.hpp"
+
+#include "cppread/detail/callable.hpp"
+
+#include <stdexcept>
+
+// =============================================================================
+//  Forward declarations
+// =============================================================================
+
+namespace cppread
+{
+    enum class Repeat
+    {
+        Cont,
+        Stop,
+    };
+
+    /**
+     * @brief Read multiple values from stdin.
+     *
+     * @param prompt The prompt.
+     * @param fn User-provided validation function, return `Repeat::Cont` to redo, `Repeat::Stop` to stop.
+     * @param delim Delimiter, only `char` so you can't use unicode.
+     *
+     * @throw std::logic_error When `fn` returns `Repeat::Stop` but value contains error.
+     * @throw User-provided-error If the `fn` throws, this function will throws as well.
+     */
+    template <Parseable... Ts, typename Fn>
+        requires(sizeof...(Ts) > 1) and (std::default_initializable<Ts> and ...)
+    std::tuple<Ts...> readRepeat(std::string_view prompt, Fn&& fn, char delim = ' ');
+
+    /**
+     * @brief Read a single value from stdin.
+     *
+     * @param prompt The prompt.
+     * @param fn User-provided validation function, return `Repeat::Cont` to redo, `Repeat::Stop` to stop.
+     * @param delim Delimiter, only `char` so you can't use unicode.
+     *
+     * @throw std::logic_error When `fn` returns `Repeat::Stop` but value contains error.
+     * @throw User-provided-error If the `fn` throws, this function will throws as well.
+     */
+    template <Parseable T, typename Fn>
+        requires std::default_initializable<T>
+    T readRepeat(std::string_view prompt, Fn&& fn, char delim = ' ');
+}
+
+// =============================================================================
+//  Implementation
+// =============================================================================
+
+namespace cppread
+{
+    template <Parseable... Ts, typename Fn>
+        requires(sizeof...(Ts) > 1) and (std::default_initializable<Ts> and ...)
+    std::tuple<Ts...> readRepeat(std::string_view prompt, Fn&& fn, char delim)
+    {
+        while (true) {
+            auto result = read<Ts...>(prompt, delim);
+            auto repeat = fn(result);
+
+            switch (repeat) {
+            case Repeat::Cont: continue;
+            case Repeat::Stop: {
+                if (result) {
+                    return std::move(result).value();
+                } else {
+                    throw std::logic_error{ "Parsed value contains error" };
+                }
+            }
+            }
+        };
+    }
+
+    template <Parseable T, typename Fn>
+        requires std::default_initializable<T>
+    T readRepeat(std::string_view prompt, Fn&& fn, char delim)
+    {
+        while (true) {
+            auto result = read<T>(prompt, delim);
+            auto repeat = fn(result);
+
+            switch (repeat) {
+            case Repeat::Cont: continue;
+            case Repeat::Stop: {
+                if (result) {
+                    return std::move(result).value();
+                } else {
+                    throw std::logic_error{ "Parsed value contains error" };
+                }
+            }
+            }
+        };
+    }
+}
+
+#endif /* end of include guard: CPPREAD_READ_REPEAT_HPP */
