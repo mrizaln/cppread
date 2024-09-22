c2d416186307486cfc123a85b1aafe3cf24833a5
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2024-09-23 00:41:51 +0700

    Update example

--- example/source/main.cpp
+++ example/source/main.cpp
@@ -1,4 +1,5 @@
 #include <cppread/read.hpp>
+#include <cppread/read_repeat.hpp>
 
 #include <iostream>
 #include <format>
@@ -8,6 +9,7 @@
 #define println(...) std::cout << std::format(__VA_ARGS__) << '\n'
 
 using cppread::read;
+using cppread::readRepeat;
 
 int main()
 try {
@@ -58,6 +60,53 @@ try {
     {
         auto string = read("Enter anything: ").value();    // `value()` will throw if error was returned
         println("anything: {}", string);
+    }
+
+    // read repeatedly until condition met (using if constexpr lambda)
+    {
+        auto value = readRepeat<int>("integer greater than 10: ", []<typename T>(T& result) {
+            if constexpr (std::same_as<T, int>) {
+                if (result <= 10) {
+                    return false;
+                } else {
+                    return true;
+                }
+            } else {
+                using cppread::Opt;
+
+                if (result != cppread::Error::EndOfFile) {
+                    return Opt<int>{ 100 };
+                }
+                return Opt<int>{};    // infinite loop if EOF is eaten :D
+            }
+        });
+
+        println("value: {}", value);
+    }
+
+    // read repeatedly until condition met (using cppread::Visit)
+    {
+        auto [value1, value2] = readRepeat<int, int>(
+            "two integer (first one must be greater than 10): ",
+            cppread::Visit{
+                [](auto& tuple) {
+                    auto [result, _] = tuple;
+                    if (result <= 10) {
+                        return false;
+                    } else {
+                        return true;
+                    }
+                },
+                [](cppread::Error error) -> cppread::Opts<int, int> {
+                    if (error != cppread::Error::EndOfFile) {
+                        return std::tuple{ 100, 100 };
+                    }
+                    return std::nullopt;    // infinite loop if EOF is eaten :D
+                },
+            }
+        );
+
+        println("value1: {} | value2: {}", value1, value2);
     }
 
 } catch (cppread::Error error) {
